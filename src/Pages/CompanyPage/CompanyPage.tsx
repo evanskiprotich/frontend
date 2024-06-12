e15046eac79adf725ae52201e97f50d99dee637d
evanskiprotich14 <evanskiprotich14@gmail.com>
2024-06-12 20:08:01 +0300

    Routes and Dashboard

---
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Tiles from "../../Components/Tiles/Tiles";
+
+interface Props {}
+
+const CompanyPage = (props: Props) => {
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  }, []);
+
+  return (
+    <>
+      {company ? (
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard><Tiles title="Company Name" subTitle={company.companyName}></Tiles></CompanyDashboard>
+        </div>
+      ) : (
+        <div>Company Not Found</div>
+      )}
+    </>
+  );
+};
+
+export default CompanyPage;
