de40321da4975b2b763d75b944af15a5f25b6e4a
evanskiprotich14 <evanskiprotich14@gmail.com>
2024-06-19 14:38:00 +0300

    API implementation

--- src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -1,25 +1,28 @@
-import React, { SyntheticEvent } from 'react'
-import DeletePortfolio from '../DeletePortfolio/DeletePortfolio';
-import { Link } from 'react-router-dom';
+import React, { SyntheticEvent } from "react";
+import { Link } from "react-router-dom";
+import DeletePortfolio from "../DeletePortfolio/DeletePortfolio";
+import { PortfolioGet } from "../../../Models/Portfolio";
 
 interface Props {
-    onPortfolioDelete: (e: SyntheticEvent) => void;
-    portfolioValue: string
+  portfolioValue: PortfolioGet;
+  onPortfolioDelete: (e: SyntheticEvent) => void;
 }
 
-const CardPortfolio = ({ onPortfolioDelete, portfolioValue}: Props) => {
+const CardPortfolio = ({ portfolioValue, onPortfolioDelete }: Props) => {
   return (
-    <>
-        <h4>{portfolioValue}</h4>
-        <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-      <Link to={`/company/${portfolioValue}/company-profile`} className="pt-6 text-xl font-bold">{portfolioValue}</Link>
+    <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
+      <Link
+        to={`/company/${portfolioValue.symbol}/company-profile`}
+        className="pt-6 text-xl font-bold"
+      >
+        {portfolioValue.symbol}
+      </Link>
       <DeletePortfolio
-        portfolioValue={portfolioValue}
+        portfolioValue={portfolioValue.symbol}
         onPortfolioDelete={onPortfolioDelete}
       />
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default CardPortfolio
+export default CardPortfolio;
